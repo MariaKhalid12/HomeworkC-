222b5cff585aa2978719b0f7fac50ae85fee9fe5
MariaKhalid12 <77403707+MariaKhalid12@users.noreply.github.com>
2021-01-13 20:03:07 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string info = "Pleas fill the following information!";
+            Console.WriteLine(info);
+
+            Console.Write("User name : ");
+            String username = Console.In.ReadLine();
+            string num;
+            int age;
+            Console.Write("Age : ");
+            num = Console.ReadLine();
+            age = Convert.ToInt32(num);
+
+            int date = 2021;
+            int res = (date - age);
+            Console.WriteLine("_______________________________________");
+            Console.WriteLine("User name : " + username);
+            Console.WriteLine("Date of Birth : " + res);
+            Console.WriteLine("_______________________________________");
+        }
+    }
+}
